d8eb1fb57d33a8ea86eb646b47a2a8bdf2928cf9
3N14C <3n14c@vk.com>
2024-05-29 23:52:55 +0800

    changes

--- src/configs/axios-config.ts
+++ src/configs/axios-config.ts
@@ -3,6 +3,6 @@ import axios from "axios";
 export const axiosInstance = axios.create({
   baseURL:
     process.env.NODE_ENV === "production"
-      ? "https://krist-web-app.vercel.app/api"
+      ? "https://boss-web-app.vercel.app/api"
       : "http://localhost:3600/api",
 });
